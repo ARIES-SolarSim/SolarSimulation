9d6ef249eee09b68e4c0067d941914087fe422dc
priyankanair2001 <priyankanair@vt.edu>
2023-05-26 13:53:06 -0400

    new ideas

--- MOVIS-base_template-main-/Assets/Scripts/Solar SIm Scripts/TideTempTwo.cs
+++ MOVIS-base_template-main-/Assets/Scripts/Solar SIm Scripts/TideTempTwo.cs
@@ -8,7 +8,8 @@ public class TideTempTwo : MonoBehaviour
     //private RotateAround Moon;
     public float minY;
     public float maxY;
-
+    public float myX;
+    public float myZ;
     private void Start()
     {
         //Moon = FindObjectOfType<RotateAround>();
@@ -18,7 +19,10 @@ public class TideTempTwo : MonoBehaviour
     void Update()
     {
         float original_number = GameObject.Find("proxyMoon").transform.position.x;
-        float newY = (Math.Abs(original_number) / 6.8f) * 4 + 3;
+        float original_number2 = GameObject.Find("proxyMoon").transform.position.z;
+        float newY = (Math.Abs(original_number) / 6.7f) * 4 + 3;
         transform.localPosition = new Vector3(5, newY, 4.67f);
+        float newNum = (float)Math.Atan(original_number2 / original_number);
+        Debug.Log(2 * Math.Cos(2 * Math.PI * newNum + Math.PI) + 5);
     }
 }
