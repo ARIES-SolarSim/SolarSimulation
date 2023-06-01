fa5f36ab94bddae920da282087541001df615a2f
S Bennett <srb@vt.edu>
2023-06-01 16:29:04 -0400

    Squashed commit of the following:
    commit e8bb566c3eceb8856e0d057024aed1c82ff362a6
    Author: S Bennett <srb@vt.edu>
    Date:   Thu Jun 1 16:27:21 2023 -0400
    
        I think it should be fine now. Clara, I hope I didn't break too many things.
    
    commit e67a78b8f8ec3012fc18dbc104cda97b3cfb3e89
    Author: S Bennett <srb@vt.edu>
    Date:   Thu Jun 1 16:25:06 2023 -0400
    
        to curse god is seeking lightning
    
        But to just be ignored?
    
    commit accc4e1ee4e7bfe1ec668f9a8e69b87f73165790
    Author: S Bennett <srb@vt.edu>
    Date:   Thu Jun 1 16:05:37 2023 -0400
    
        Consider this a prayer on deaf ears.
    
    commit 44def04f9b95fd17e29059c2599bfe44289f2eb3
    Author: S Bennett <srb@vt.edu>
    Date:   Thu Jun 1 15:52:14 2023 -0400
    
        Script Box Created
    
        I had to throw out some merge changes. Working to fix it now

---
+++ MOVIS-base_template-main-/Assets/Scripts/Solar Sim Scripts/UIScripts/ScriptManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptManager : MonoBehaviour
+{
+    private string[] currentScript = { "1st Line", "expanded 1", "2nd Line", "Expanded 2" };
+    private int scriptIter = 0;
+
+    public TextAsset[] csvFiles;
+    private string[][] agedScripts;
+
+    //Font Uses 
+    private int fontIter = 0;
+    public int[] sizePresets = { 10, 11, 12 };
+    private Text tc;
+
+    private int ageIter = 0;
+
+    private bool expanded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tc = this.GetComponent<Text>();
+        ReadCSV();
+        tc.text = currentScript[0];
+        //Handle reading here     
+    }
+    
+    public void Forward()
+    {   
+        if (currentScript.Length > scriptIter + 2)
+        {
+            scriptIter += 2;
+            expanded = false;
+            tc.text = currentScript[scriptIter];
+        }
+    }
+
+    public void Back()
+    {
+        if (scriptIter > 0)
+        {
+            scriptIter -= 2;
+            expanded = false;
+            tc.text = currentScript[scriptIter];
+        }
+    }
+
+    /// <summary>
+    /// This function will toggle whether or not the text contains it's expanded form
+    /// The expanded form data is held in the odd indexed arrays 
+    /// </summary>
+    public void Expand()
+    {
+        if (expanded){ // It's already expanded out -> Shrink
+            expanded = false;
+            tc.text = currentScript[scriptIter];
+            // Simply generate the evens again
+        }
+
+        else // It's not expanded -> Expand
+        { 
+            expanded = true;
+            tc.text += "\n"+ currentScript[scriptIter + 1];
+            // Add the associated Odd Script
+        }
+    }
+    
+
+    public void Age()
+    {   
+        ageIter = (ageIter + 1) % agedScripts.Length; // Loop through the scripts 
+        currentScript = agedScripts[ageIter]; // change currentScript to the right age 
+        scriptIter = 0; // Restart the script
+        tc.text = currentScript[scriptIter]; // Display changes 
+        expanded = false; // Mark that it is not expanded 
+    }
+
+    public void FontChange()
+    {
+        tc.fontSize = sizePresets[fontIter % sizePresets.Length];
+        // Iterate through the size presets, set up at the stop of the currentScript
+        fontIter = (fontIter + 1 % sizePresets.Length);
+    }
+
+    private void ReadCSV()
+    {
+        for (int i = 0; i < csvFiles.Length; i++)
+        {
+            agedScripts[i] = csvFiles[i].text.Split(new string[] { ",", "\n" }, System.StringSplitOptions.None);
+        }
+    }
+}
