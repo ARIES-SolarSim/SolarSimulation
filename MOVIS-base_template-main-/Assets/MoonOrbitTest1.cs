2e8715c091abdad17b3e81982efb637126896b51
LeahI24 <>
2023-06-12 15:47:18 -0400

    tilted orbit in room 2
    :( physics bye bye

--- MOVIS-base_template-main-/Assets/MoonOrbitTest1.cs
+++ MOVIS-base_template-main-/Assets/MoonOrbitTest1.cs
@@ -8,6 +8,7 @@ public class MoonOrbitTest1 : MonoBehaviour
     public float radius;
     public float radiusSpeed;
     public float rotationSpeed;
+    private float unit = .001f;
 
     private Vector3 axis;
     private Vector3 desiredPosition;
@@ -15,14 +16,22 @@ public class MoonOrbitTest1 : MonoBehaviour
     void Start()
     {
         transform.position = (transform.position - center.position).normalized * radius + center.position;
-        axis = Vector3.up;
+        axis = Vector3.down;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
-        desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        
+        Vector3 centerCopy = center.position;
+        centerCopy = new Vector3(centerCopy.x, centerCopy.y + unit, centerCopy.z);
+        transform.RotateAround(centerCopy, axis, rotationSpeed * Time.deltaTime);
+        desiredPosition = (transform.position - center.position).normalized * radius + centerCopy;
         transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+
+        if (Mathf.Abs(transform.position.y) >= 4)
+        {
+            unit = unit * -1f;
+        }
     }
 }
