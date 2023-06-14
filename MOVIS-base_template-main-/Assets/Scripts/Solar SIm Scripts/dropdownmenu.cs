681bd54f2b79f7961e196117a596c1274dec4755
priyankanair2001 <priyankanair@vt.edu>
2023-06-14 16:39:14 -0400

    made some fixes

--- MOVIS-base_template-main-/Assets/Scripts/Solar SIm Scripts/dropdownmenu.cs
+++ MOVIS-base_template-main-/Assets/Scripts/Solar SIm Scripts/dropdownmenu.cs
@@ -13,38 +13,42 @@ public class dropdownmenu : MonoBehaviour
     
     public void DropdownSample(int index)
     {
-        image = GameObject.Find("Image");
-        tide = GameObject.Find("Tide");
+        //image = GameObject.Find("Image");
+        //tide = GameObject.Find("Tide");
+        //United Kingdom
         if (index == 0)
         {
             tide.GetComponent<TideTempTwo>().enabled = false;
             tide.GetComponent<TideTempTwo>().enabled = true;
-            image.GetComponent<RectTransform>().anchoredPosition  = new Vector3(-385, -30, 0);
+            image.GetComponent<RectTransform>().anchoredPosition  = new Vector3(-420, -45, 0);
             tide.GetComponent<TideTempTwo>().myX = -5.401577f;
             tide.GetComponent<TideTempTwo>().myZ = -3.792488f;
-            tide.GetComponent<TideTempTwo>().minY = 2+(51.5072f/90)*(3);
+            tide.GetComponent<TideTempTwo>().minY = 8 + (51.5072f/90)*(12);
 
         }
+        //Egypt
         if (index == 1)
         {
             tide.GetComponent<TideTempTwo>().enabled = false;
             tide.GetComponent<TideTempTwo>().enabled = true;
-            image.GetComponent<RectTransform>().anchoredPosition = new Vector3(-330, -75, 0);
+            image.GetComponent<RectTransform>().anchoredPosition = new Vector3(-380, -110, 0);
             tide.GetComponent<TideTempTwo>().myX = -0.730737f;
             tide.GetComponent<TideTempTwo>().myZ = -6.559422f;
-            tide.GetComponent<TideTempTwo>().minY = 2 + (30.0444f / 90) * (3);
+            tide.GetComponent<TideTempTwo>().minY = 8 + (30.0444f / 90) * (12);
 
         }
+        //Japan
         if (index == 2)
         {
             tide.GetComponent<TideTempTwo>().enabled = false;
             tide.GetComponent<TideTempTwo>().enabled = true;
-            image.GetComponent<RectTransform>().anchoredPosition = new Vector3(-270, 32, 0);
+            image.GetComponent<RectTransform>().anchoredPosition = new Vector3(-280, 40, 0);
             tide.GetComponent<TideTempTwo>().myX = 3.621747f;
             tide.GetComponent<TideTempTwo>().myZ = 5.517513f;
-            tide.GetComponent<TideTempTwo>().minY = 2 + (35.6762f / 90) * (3);
+            tide.GetComponent<TideTempTwo>().minY = 8 + (35.6762f / 90) * (12);
 
         }
+        //India
         if (index == 3)
         {
             //Destroy(tide.GetComponent<TideTempTwo>());
@@ -55,7 +59,35 @@ public class dropdownmenu : MonoBehaviour
             image.GetComponent<RectTransform>().anchoredPosition = new Vector3(-270, -50, 0);
             tide.GetComponent<TideTempTwo>().myX = 4.203157f;
             tide.GetComponent<TideTempTwo>().myZ = -5.088562f;
-            tide.GetComponent<TideTempTwo>().minY = 2 + (28.6139f / 90) * (3);
+            tide.GetComponent<TideTempTwo>().minY = 8 + (28.6139f / 90) * (12);
+
+        }
+        //North Pole
+        if (index == 4)
+        {
+            //Destroy(tide.GetComponent<TideTempTwo>());
+            //tide.AddComponent<TideTempTwo>();
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            tide.GetComponent<TideTempTwo>().enabled = false;
+            tide.GetComponent<TideTempTwo>().enabled = true;
+            image.GetComponent<RectTransform>().anchoredPosition = new Vector3(-270, -50, 0);
+            tide.GetComponent<TideTempTwo>().myX = 4.203157f;
+            tide.GetComponent<TideTempTwo>().myZ = -5.088562f;
+            tide.GetComponent<TideTempTwo>().minY = 8 + (90.00f / 90) * (12);
+
+        }
+        //Singapore
+        if (index == 5)
+        {
+            //Destroy(tide.GetComponent<TideTempTwo>());
+            //tide.AddComponent<TideTempTwo>();
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            tide.GetComponent<TideTempTwo>().enabled = false;
+            tide.GetComponent<TideTempTwo>().enabled = true;
+            image.GetComponent<RectTransform>().anchoredPosition = new Vector3(-270, -50, 0);
+            tide.GetComponent<TideTempTwo>().myX = 4.203157f;
+            tide.GetComponent<TideTempTwo>().myZ = -5.088562f;
+            tide.GetComponent<TideTempTwo>().minY = 8 + (0.0f / 90) * (12);
 
         }
 
