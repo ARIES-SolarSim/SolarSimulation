95b480cd7f6827d445a9332eb58e1b5a863d886a
cjmcdd <89484559+cjmcdd@users.noreply.github.com>
2023-06-15 12:55:07 -0400

    Moon orbit is looking fancy and tilted
    and elliptical!

--- MOVIS-base_template-main-/Assets/MoonOrbitTest1.cs
+++ MOVIS-base_template-main-/Assets/MoonOrbitTest1.cs
@@ -3,35 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoonOrbitTest1 : MonoBehaviour
-{
-    public Transform center;
-    public float radius;
-    public float radiusSpeed;
+{public Transform center;
+    public float semiMajorAxis;
+    public float semiMinorAxis;
+    public float orbitSpeed;
     public float rotationSpeed;
-    private float unit = .001f;
+    public float tiltAngle;
 
-    private Vector3 axis;
-    private Vector3 desiredPosition;
+    private float currentOrbitAngle = 0f;
+    private float currentRotationAngle = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = (transform.position - center.position).normalized * radius + center.position;
-        axis = Vector3.down;
+        // Set initial position based on the desired semi-major and semi-minor axes
+        float x = center.position.x + semiMajorAxis * Mathf.Cos(tiltAngle * Mathf.Deg2Rad);
+        float z = center.position.z + semiMinorAxis * Mathf.Sin(tiltAngle * Mathf.Deg2Rad);
+        transform.position = new Vector3(x, center.position.y, z);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        Vector3 centerCopy = center.position;
-        centerCopy = new Vector3(centerCopy.x, centerCopy.y + unit, centerCopy.z);
-        transform.RotateAround(centerCopy, axis, rotationSpeed * Time.deltaTime);
-        desiredPosition = (transform.position - center.position).normalized * radius + centerCopy;
-        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+       // Calculate the new position on the elliptical orbit
+        float x = center.position.x + semiMajorAxis * Mathf.Cos(currentOrbitAngle);
+        float z = center.position.z + semiMinorAxis * Mathf.Sin(currentOrbitAngle);
+        Vector3 newPosition = new Vector3(x, center.position.y, z);
 
-        if (Mathf.Abs(transform.position.y) >= 4)
+        // Rotate around the center and update the position
+        transform.RotateAround(center.position, Vector3.up, orbitSpeed * Time.deltaTime);
+        transform.position = newPosition;
+
+        // Tilt the orbit by rotating around the x-axis
+        Quaternion tiltRotation = Quaternion.Euler(tiltAngle, 0f, 0f);
+        transform.position = tiltRotation * (transform.position - center.position) + center.position;
+
+        // Update the current angle based on the rotation speed
+        currentOrbitAngle += orbitSpeed * Time.deltaTime;
+        currentRotationAngle += rotationSpeed * Time.deltaTime;
+
+        // If the current angle exceeds a full circle, reset it
+        if (currentOrbitAngle >= 2 * Mathf.PI)
         {
-            unit = unit * -1f;
+            currentOrbitAngle -= 2 * Mathf.PI;
         }
+
+        // Apply rotation to the moon's own axis
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
     }
 }
