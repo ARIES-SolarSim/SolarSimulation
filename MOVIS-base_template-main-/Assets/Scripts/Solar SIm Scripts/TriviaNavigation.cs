560f21a4b2e714e46a47250adfdca49e7064c761
Clara McDaniel <89484559+cjmcdd@users.noreply.github.com>
2023-08-14 14:06:51 -0400

    more trivia progress (i am no longer being stupid)

--- MOVIS-base_template-main-/Assets/Scripts/Solar SIm Scripts/TriviaNavigation.cs
+++ MOVIS-base_template-main-/Assets/Scripts/Solar SIm Scripts/TriviaNavigation.cs
@@ -30,21 +30,24 @@ public class TriviaNavigation : MonoBehaviour
             }
             else if(i == 1)
             {
+                planets[i].transform.localPosition = new Vector3(0.5166248f, 0.04224792f, 0.4812764f);
             }
             else if (i == 2)
             {
-
+                planets[i].transform.localPosition = new Vector3(-0.4128868f, -0.007746019f, -0.9266148f);
             }
             else if (i == 3)
             {
-
+                planets[i].transform.localPosition = new Vector3(-1.121581f, 0.04553582f, 0.08777341f);
             }
             else if (i == 4)
             {
+                planets[i].transform.localPosition = new Vector3(0.08973803f, -0.000822f, 1.822217f);
 
             }
             else if (i == 5)
             {
+                planets[i].transform.localPosition = new Vector3(1.956572f, -0.03500602f, 1.624338f);
 
             }
             else if (i == 6)
